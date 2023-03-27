5178618d4e101acf258b74b184e46ce2fea09a4f
p-llisaso <p-llisaso@P-LLISASO.neoris.cxnetworks.net>
2023-03-27 16:23:28 +0200

    1 commit

---
+++ prueba1/src/com/rf/demo/Principal.java
@@ -0,0 +1,13 @@
+package com.rf.demo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		
+		System.out.println();
+
+	}
+
+}
